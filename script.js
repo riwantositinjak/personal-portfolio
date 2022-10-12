c9c64d1ba4a074d91a27a54ba040942c408067be
riwantositinjak <riwanto1212@gmail.com>
2022-10-12 17:36:44 +0700

    editing navigation bar animate

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 const slide = () => {
-    const icon = document.querySelector(".icon");
-    const navigation = document.querySelector(".navigation");
-    const navLinks = document.querySelectorAll(".navigation li");
-  
-    //toggle nav
-    icon.addEventListener("click", () => {
-      navigation.classList.toggle("nav-active");
-  
-      // animate Links
-      navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = "";
-        } else {
-          link.style.animation = `navigationLinkAnimate 0.5s ease forwards ${
-            index / 7 + 1
-          }s`;
-        }
-      });
-  
-      // icon animation
-      icon.classList.toggle("toggle");
+  const icon = document.querySelector(".icon");
+  const navigation = document.querySelector(".navigation");
+  const navLinks = document.querySelectorAll(".navigation li");
+
+  //toggle nav
+  icon.addEventListener("click", () => {
+    navigation.classList.toggle("nav-active");
+
+    // animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navigationLinkAnimate 0.5s ease forwards ${
+          index / 7 + 1
+        }s`;
+      }
     });
-  };
-  
-  slide();
+
+    // icon animation
+    icon.classList.toggle("toggle");
+  });
+};
+
+slide();
